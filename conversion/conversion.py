5f10c01c2c9b196d1a9f676e3dd7ae97661fe87f
shawn <nanttery@gmail.com>
2017-02-19 12:42:44 +0800

    modify syntax to adapt to python3

--- conversion/conversion.py
+++ conversion/conversion.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 from flask import Flask, request
-import convertors
+from . import convertors
 
 
 app = Flask(__name__)
--- conversion/convertors.py
+++ conversion/convertors.py
@@ -1,33 +1,33 @@
 #-*- coding: utf-8 -*-
 
 def chinese2arabic(number, strict=False):
-  u"""
+  """
   Convert chinese numbers to arabic numbers, both support tranditional 
   or simplified chinese numbers, for examples:
-  >>> chinese2arabic(u'四十二')
+  >>> chinese2arabic('四十二')
   42
-  >>> chinese2arabic(u'五百二十')
+  >>> chinese2arabic('五百二十')
   520
-  >>> chinese2arabic(u'两千零一十七')
+  >>> chinese2arabic('两千零一十七')
   2017
-  >>> chinese2arabic(u'一亿四千八百万bad零一千八百六十二')
+  >>> chinese2arabic('一亿四千八百万bad零一千八百六十二')
   148001862
-  >>> chinese2arabic(u'一亿四千八百万bad零一千八百六十二', True)
+  >>> chinese2arabic('一亿四千八百万bad零一千八百六十二', True)
   Traceback (most recent call last):
   ...
   ValueError: Illegal input
-  >>> chinese2arabic(u'')
+  >>> chinese2arabic('')
   Traceback (most recent call last):
   ...
   AssertionError: Illegal input
   """
   assert number and len(number) > 0, 'Illegal input'
   number = simplify2lower(simplify(number))
-  number = number.replace(u'零', '')
-  number = number.replace(u'〇', '')
-  place_dict = {u'一': '1', u'二': '2', u'两': '2', u'三': '3', u'四': '4',
-   u'五': '5', u'六': '6', u'七': '7', u'八': '8', u'九': '9'}
-  paddings = {u'十': 1, u'百': 2, u'千': 3, u'万': 4, u'亿': 8}
+  number = number.replace('零', '')
+  number = number.replace('〇', '')
+  place_dict = {'一': '1', '二': '2', '两': '2', '三': '3', '四': '4',
+   '五': '5', '六': '6', '七': '7', '八': '8', '九': '9'}
+  paddings = {'十': 1, '百': 2, '千': 3, '万': 4, '亿': 8}
 
   res = ''
   add_padding = 0
@@ -39,13 +39,13 @@ def chinese2arabic(number, strict=False):
         if strict: raise ValueError('Illegal input')
         else: continue
 
-      if digit == u'亿':
+      if digit == '亿':
         add_padding = 0
       padding = padding + add_padding
       res = res.zfill(padding)
-      if digit == u'万':
+      if digit == '万':
         add_padding = add_padding + 4
-      if digit == u'亿':
+      if digit == '亿':
         add_padding = 8
     else:
       res = sim + res
@@ -57,36 +57,36 @@ def chinese2arabic(number, strict=False):
   return int(res)
 
 def simplify2lower(number):
-  u"""
+  """
   Convert uppercase chinese number to lowercase chinese number,
   for examples:
-  >>> print simplify2lower(u'壹')
-  一
-  >>> print simplify2lower(u'壹亿肆仟捌佰万零壹仟捌佰陆拾贰')
-  一亿四千八百万零一千八百六十二
+  >>> simplify2lower('壹')
+  '一'
+  >>> simplify2lower('壹亿肆仟捌佰万零壹仟捌佰陆拾贰')
+  '一亿四千八百万零一千八百六十二'
   """
 
-  intab = u'壹贰叁肆伍陆柒捌玖拾佰仟'
+  intab = '壹贰叁肆伍陆柒捌玖拾佰仟'
   intab = [ord(c) for c in intab]
-  outab = u'一二三四五六七八九十百千'
+  outab = '一二三四五六七八九十百千'
   trantab = dict(zip(intab, outab))
 
   return number.translate(trantab)
 
 
 def simplify(number):
-  u"""
+  """
   Convert traditional chinese uppercase number 
   to simplied chinese uppercase number,
   for examples:
-  >>> print simplify(u'貳')
-  贰
+  >>> simplify('貳')
+  '贰'
 
   """
 
-  intab = u'貳參陸萬億'
+  intab = '貳參陸萬億'
   intab = [ord(c) for c in intab]
-  outab = u'贰叁陆萬億'
+  outab = '贰叁陆萬億'
 
   trantab = dict(zip(intab, outab))
 
