0eeeab064ad170b7503ac04b3a3934c337aac474
shawn <nanttery@gmail.com>
2017-03-02 11:13:44 +0800

    find my ip

--- conversion/conversion.py
+++ conversion/conversion.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from . import convertors
 from . import cros
 
@@ -11,23 +10,35 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def home():
-  return 'home'
-  return app.send_static_file('index.html')
+    return 'home', 200
+    return app.send_static_file('index.html')
 
 
 @app.route('/chinese2arabic/<number>')
 @cros.crossdomain(origin='*')
 def chinese2arabic(number):
-  """
-  Convert chinese numbers to arabic numbers, only integer supported.
-  """
-  strict = request.args.get('strict', False)
-  try:
-    arabic = convertors.chinese2arabic(number, strict)
+    """
+    Convert chinese numbers to arabic numbers, only integer supported.
+    """
+    strict = request.args.get('strict', False)
+    try:
+        arabic = convertors.chinese2arabic(number, strict)
+        format = request.args.get('format', None)
+        if format == 'json':
+            return jsonify({'number': int(arabic)}), 200
+            # return "{'number': %d}" % int(arabic), 200
+        else:
+            return str(arabic), 200
+    except:
+        return 'Conversion failed', 200
+
+
+@app.route('/myip')
+@cros.crossdomain(origin='*')
+def getclientip():
     format = request.args.get('format', None)
     if format == 'json':
-      return "{'number': %d}" % int(arabic)
+        return jsonify(ip=request.remote_addr), 200
     else:
-      return str(arabic)
-  except:
-    return 'Conversion failed'
+        return request.remote_addr, 200
+
