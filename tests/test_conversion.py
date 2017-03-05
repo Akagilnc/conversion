0f7e505529fe505f33f25fca0af03dca48ba738c
DeronLee <akagilnc@gmail.com>
2017-03-05 16:48:33 +0800

    fixed float part bug

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -27,6 +27,10 @@ class ConversionTestCase(unittest.TestCase):
     assert b'Conversion failed' in rv.data
     rv = self.app.get('/chinese2arabic/两千零一十七点五零五')
     assert b'2017.505' in rv.data
+    rv = self.app.get('/chinese2arabic/一亿两千零一十七点五零零五八')
+    assert b'100002017.50058' in rv.data
+    rv = self.app.get('/chinese2arabic/两千零一十七点五零五一九')
+    assert b'2017.50519' in rv.data
 
 
 if __name__=='__main__':
